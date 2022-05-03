c82f78e0212a6cdb5dacb3b619d6055ab11b17cc
Matt Porter <mattp773@gmail.com>
2022-05-03 13:17:41 -0400

    modified random_array_gen to print straight to file and wrote working sequential code for at least small data sets

--- random_array_gen.c
+++ random_array_gen.c
@@ -12,11 +12,19 @@ int main(int argc, char** argv)
     srand(time(NULL));
     float n = RAND_MAX;
     float x;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 1000; j++) {
+    int i, j;
+    FILE* fp = fopen("input.txt", "w");
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < 1000; j++) {
             x = (100 * (rand() / n));
-            printf("%f\n", x);
+            if ((j + i) == (1003)) {
+                fprintf(fp, "%f", x);
+            } else {
+                fprintf(fp, "%f\n", x);
+            }
         }
     }
+    fclose(fp);
     return 0;
 }
