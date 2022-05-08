07722dd4287d6cba72e37bd17baa8e97cdc506d5
Matt <mattp773@gmail.com>
2022-05-08 17:34:55 -0400

    updated sequential_2.c to work (hopefully) up to 1 billion

--- sequential_2.c
+++ sequential_2.c
@@ -2,32 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <float.h>
 
-int main(int argc, char** argv)
+typedef struct primes {
+    long number;
+    bool is_prime;
+}primes;
+
+int main(int argc, char *argv[])
 {
     if (argc != 2) {
         printf("Usage: %s <Number of elements>\n", argv[0]);
         return 1;
     }
     
-    int N = atoi(argv[1]); //upper limit
-    int sum = 2;
-    bool is_prime;
-    int i, j;
+    long N = atol(argv[1]); //upper limit
+    double sum = 0;
+    struct primes *array = malloc(N * sizeof(primes));
+    long i, j;
 
     //i is the current number, all primes must be less than N
-    for(i = 3; i < N; i++) {
-        is_prime = true;
-        for(j = 2; j <= (int)sqrt(i); j++) {
-            if(i % j == 0) {
-                is_prime = false;
-                break;
+    for(i = 2; i < N; i++) {
+        array[i].is_prime = true;
+            for(j = 2; j <= (long)sqrt((float)i); j++) {
+                if(i % j == 0) {
+                    array[i].is_prime = false;
+                    break;
+                }
+            }
+            if(array[i].is_prime) {
+                sum += i;
             }
         }
-        if(is_prime) {
-            sum += i;
-        }
+    if(printf("Sum of primes less than %ld = %.0f\n", N, sum) < 0) {
+        printf("error printing sum\n");
+        return 1;
     }
-    printf("sum of primes less than %d = %d\n", N, sum);
     return 0;
 }
